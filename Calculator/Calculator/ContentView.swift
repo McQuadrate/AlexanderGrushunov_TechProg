4781a7ef86b2c10257c6bd38db95cccd50a85878
SecondThundeR <awayfromgalaxy@gmail.com>
2022-04-02 09:52:02 +0300

    Add Button Styles

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -7,6 +7,69 @@
 
 import SwiftUI
 
+struct OtherButtonsBackground: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(Color("Other").opacity(0.8))
+            .foregroundColor(.white)
+            .border(Color("Border"), width: 3)
+            .clipShape(Rectangle())
+            .font(.system(size: 28))
+    }
+}
+
+struct BracketsButtonsBackground: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(Color("Brackets").opacity(0.8))
+            .foregroundColor(.white)
+            .border(Color("Border"), width: 3)
+            .clipShape(Rectangle())
+            .font(.system(size: 28))
+    }
+}
+
+
+struct DigitsButtonsBackground: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(Color("Digits").opacity(0.8))
+            .foregroundColor(.white)
+            .border(Color("Border"), width: 3)
+            .clipShape(Rectangle())
+            .font(.system(size: 28))
+    }
+}
+
+
+struct ResultButtonBackground: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(Color("Result").opacity(0.8))
+            .foregroundColor(.white)
+            .border(Color("Border"), width: 3)
+            .clipShape(Rectangle())
+            .font(.system(size: 28))
+    }
+}
+
+
+struct ClearButtonBackground: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(Color("Clear").opacity(0.8))
+            .foregroundColor(.white)
+            .border(Color("Border"), width: 3)
+            .clipShape(Rectangle())
+            .font(.system(size: 28))
+    }
+}
+
 struct ContentView: View {
     @State private var calcStr: String = ""
     @State private var lastRes: String = ""
