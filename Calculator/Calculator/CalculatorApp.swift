5ecc24636c5f15b228e3623c524fee6d808e3937
Владислав <vladgrom2206.com@gmail.com>
2022-04-02 09:40:22 +0300

    Init Xcode Project

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Владислав on 02.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
