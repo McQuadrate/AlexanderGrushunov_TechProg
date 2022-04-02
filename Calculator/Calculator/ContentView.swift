28ea73a84cef6d91f5d16b0ecf750700193f6e7b
SecondThundeR <awayfromgalaxy@gmail.com>
2022-04-02 09:46:16 +0300

    Add calculator @State variables

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var calcStr: String = ""
+    @State private var lastRes: String = ""
+
     var body: some View {
         Text("Hello, world!")
             .padding()
